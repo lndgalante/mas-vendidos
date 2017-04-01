4a6b7f876439a325a36bdbfd112fb25c79127bb2
Fenwil <lndgalante@gmail.com>
2017-04-01 04:41:59 -0300

    First commit 🎉

---
+++ src/content.js
@@ -0,0 +1,41 @@
+// Find the most selled one on the list and save it to mostSelled
+
+const products = [...document.querySelectorAll('.extra-info-sold')];
+
+const solds = products
+  .map(product => product.innerText.replace(' vendidos', ''))
+  .map(price => parseInt(price));
+
+const mostSelled = solds.reduce((a, b) => Math.max(a, b));
+
+// Find and Style the entire row where the most selled lives
+
+const row = [...document.querySelectorAll('.rowItem')]
+  .find(row => row.innerText.includes(mostSelled));
+  
+row[0].style.boxShadow = '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)';
+row[0].style.backgroundColor = '#F9ED69';
+row[0].style.borderColor = '#F08A5D';
+row[0].style.borderWidth = '4px';
+
+// Find the element where the mostSelled value lives
+
+const item = products.find(product => product.innerText.includes(mostSelled));
+
+item.style.borderRadius = '4px 0px 0px 4px';
+item.style.textTransform = 'uppercase';
+item.style.backgroundColor = '#212121';
+item.style.padding = '2px 0px 2px 4px';
+item.style.marginLeft = '-4px';
+item.style.lineHeight = '1.2';
+item.style.fontWeight = '700';
+item.style.fontSize = '18px';
+item.style.color = '#FFF';
+
+// Scroll to where that item (row) is
+
+const productHeight = document.querySelector('.rowItem').offsetHeight;
+const itemRect = item.getBoundingClientRect();
+const offset = itemRect.top + window.scrollY - productHeight;
+
+window.scrollTo(0, offset);
