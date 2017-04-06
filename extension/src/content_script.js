90ad5e666f41434dedc47d89a56d1d1994fbe4f2
Fenwil <lndgalante@gmail.com>
2017-04-06 20:11:45 -0300

    Order the quantity of sold products descendly

--- extension/src/content_script.js
+++ extension/src/content_script.js
@@ -7,50 +7,41 @@ const viewMode = document.querySelector('.ico');
 const viewModeActive = viewMode.className.includes('list selected') ? 'list' : 'gallery';
 
 if (viewModeActive === 'gallery') {
+  // imagesViewer.map(imageViewer => imageViewer.innerHTML = null);
   soldQuantitySelector = '.sold-quantity';
+  locationSelector = '.info-shipping';
+  listSelector = '.gallery-large';
   itemHeight = 90;
 } else if (viewModeActive === 'list') {
   soldQuantitySelector = '.extra-info-sold';
+  locationSelector = '.extra-info-location';
+  listSelector = '.list-view';
   itemHeight = 60;
 }
 
-// Find the most selled one on the list and save it to mostSelled
-const infoSolds = [...document.querySelectorAll(soldQuantitySelector)];
+// Add "0 vendidos" to those products that didn't sell nothing yet
+const rowItems = [...document.querySelectorAll('.results-item')];
 
-const unitsSold = infoSolds
-  .map(infoSold => infoSold.innerText.replace(/\D/g, ''))
-  .map(soldQuantity => isNaN(soldQuantity) ? 0 : parseInt(soldQuantity));
-
-const mostSelled = unitsSold.reduce((a, b) => Math.max(a, b));
-
-// Find and Style the entire row where the mostSelled value lives
-const rowItems = [...document.querySelectorAll('.rowItem')];
-
-const rowItemElement = rowItems.find(row => {
+rowItems.forEach(row => {
   const info = row.querySelector(soldQuantitySelector);
-  return info ? info.innerText.includes(mostSelled) : false;
+  if (!info) {
+    row
+      .querySelector(locationSelector)
+      .insertAdjacentHTML('beforebegin', `<li class="${soldQuantitySelector.replace('.', '')}">0 vendidos</li>`);
+  }
 });
 
-rowItemElement.style.boxShadow = '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)';
-rowItemElement.style.backgroundColor = '#F9ED69';
-rowItemElement.style.borderColor = '#F08A5D';
-rowItemElement.style.borderWidth = '4px';
-rowItemElement.style.margin = '4px';
+// Sort all the quantity of sold products descendly
+const rowItemsUpdated = [...document.querySelectorAll('.results-item')];
+const rowItemsSorted = rowItemsUpdated.sort((a, b) => {
+  const valueA = a.querySelector(soldQuantitySelector).innerText.replace(/\D/g, '');
+  const valueB = b.querySelector(soldQuantitySelector).innerText.replace(/\D/g, '');
+  return valueA - valueB;
+});
 
-// Find the HTML element where the most selled value lives
-const mostSelledElement = infoSolds.find(product => product.innerText.includes(mostSelled));
+// Lets clean all items inside the list
+const listView = document.querySelector(listSelector);
+listView.innerHTML = 'null';
 
-mostSelledElement.style.borderRadius = '4px 0px 0px 4px';
-mostSelledElement.style.textTransform = 'uppercase';
-mostSelledElement.style.backgroundColor = '#212121';
-mostSelledElement.style.padding = '2px 0px 2px 4px';
-mostSelledElement.style.marginLeft = '-4px';
-mostSelledElement.style.lineHeight = '1.2';
-mostSelledElement.style.fontWeight = '700';
-mostSelledElement.style.fontSize = '18px';
-mostSelledElement.style.color = '#FFF';
-
-// Scroll to where that mostSelledElement element is
-const mostSelledElementRect = rowItemElement.getBoundingClientRect();
-const offset = mostSelledElementRect.top + window.scrollY - itemHeight;
-window.scrollTo(0, offset);
+// Now lets insert all the items ordered descendendly
+rowItemsSorted.forEach(row => listView.insertAdjacentElement('afterbegin', row));
