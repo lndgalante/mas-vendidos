771258bf980ebd74406af6e4acdd6c0d1343f03c
Micael Robles <micael.robles@gmail.com>
2017-04-26 00:03:03 -0300

    Pause plugin via popup

---
+++ src/background.js
@@ -0,0 +1,16 @@
+var enabled = true;
+
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request == "getState") {
+            sendResponse(enabled);
+        } else {
+            enabled = request == "Activar";
+            enabled ? chrome.browserAction.setIcon({
+                path: "assets/mercado-libre_128.png"
+            }) : chrome.browserAction.setIcon({
+                path: "assets/mercado-libre-gray_128.png"
+            })
+        }
+    }
+);
