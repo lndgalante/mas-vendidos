0a56a9ac677ae7d88bceabbcdbadb2dc6d712c35
Fenwil <lndgalante@gmail.com>
2017-05-20 12:40:45 -0300

    Add lint-staged, xo and prettier

--- src/background.js
+++ src/background.js
@@ -1,12 +1,14 @@
-var enabled = true
+let enabled = true
+
+/* global chrome */
+/* eslint no-undef: "error" */
 
 chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
-  if (request === 'getState') {
-    sendResponse(enabled)
-  } else {
-    enabled = request === 'Activar'
-    enabled
-      ? chrome.browserAction.setIcon({ path: 'assets/mercado-libre_128.png' })
-      : chrome.browserAction.setIcon({ path: 'assets/mercado-libre-disabled_128.png' })
-  }
+	if (request === 'getState') {
+		sendResponse(enabled)
+	} else {
+		enabled = request === 'Activar'
+		const path = enabled ? 'assets/mercado-libre_128.png' : 'assets/mercado-libre-disabled_128.png'
+		chrome.browserAction.setIcon({path})
+	}
 })
