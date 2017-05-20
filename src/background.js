d288bf61b0fef46a2602c93334935fef9e70a734
Fenwil <lndgalante@gmail.com>
2017-05-20 13:44:54 -0300

    Fix prettier and xo integration

--- src/background.js
+++ src/background.js
@@ -9,6 +9,6 @@ chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
 	} else {
 		enabled = request === 'Activar'
 		const path = enabled ? 'assets/mercado-libre_128.png' : 'assets/mercado-libre-disabled_128.png'
-		chrome.browserAction.setIcon({path})
+		chrome.browserAction.setIcon({ path })
 	}
 })
