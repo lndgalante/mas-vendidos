6c2841dd4894e865afc018c44bf0be6c74e816a4
Fenwil <lndgalante@gmail.com>
2017-05-20 13:50:49 -0300

    New release

--- dist/src/background.js
+++ dist/src/background.js
@@ -1,1 +1,11 @@
-"use strict";var enabled=!0;chrome.extension.onMessage.addListener(function(e,s,t){"getState"===e?t(enabled):(enabled="Activar"===e,enabled?chrome.browserAction.setIcon({path:"assets/mercado-libre_128.png"}):chrome.browserAction.setIcon({path:"assets/mercado-libre-disabled_128.png"}))});
+'use strict'
+let enabled = !0
+chrome.extension.onMessage.addListener((e, a, s) => {
+  if (e === 'getState') {
+    s(enabled)
+  } else {
+    enabled = e === 'Activar'
+    const n = enabled ? 'assets/mercado-libre_128.png' : 'assets/mercado-libre-disabled_128.png'
+    chrome.browserAction.setIcon({ path: n })
+  }
+})
