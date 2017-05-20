bb60c27e542d2620c658ae7908519f648944a0a6
Fenwil <lndgalante@gmail.com>
2017-05-20 13:46:38 -0300

    Fix prettier and xo integration

--- src/background.js
+++ src/background.js
@@ -4,11 +4,11 @@ let enabled = true
 /* eslint no-undef: "error" */
 
 chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
-	if (request === 'getState') {
-		sendResponse(enabled)
-	} else {
-		enabled = request === 'Activar'
-		const path = enabled ? 'assets/mercado-libre_128.png' : 'assets/mercado-libre-disabled_128.png'
-		chrome.browserAction.setIcon({ path })
-	}
+  if (request === 'getState') {
+    sendResponse(enabled)
+  } else {
+    enabled = request === 'Activar'
+    const path = enabled ? 'assets/mercado-libre_128.png' : 'assets/mercado-libre-disabled_128.png'
+    chrome.browserAction.setIcon({ path })
+  }
 })
--- src/content-script.js
+++ src/content-script.js
@@ -2,52 +2,52 @@
 /* eslint no-undef: "error" */
 
 chrome.runtime.sendMessage('getState', state => {
-	if (state) {
-		sortItems()
-	}
+  if (state) {
+    sortItems()
+  }
 })
 
 function sortItems() {
   // Check if active view mode is List or Gallery mode
-	let soldQuantitySelector = ''
-	let locationSelector = ''
-	let listSelector = ''
-	const viewMode = document.querySelector('.ico')
-	const viewModeActive = viewMode.className.includes('list selected') ? 'list' : 'gallery'
+  let soldQuantitySelector = ''
+  let locationSelector = ''
+  let listSelector = ''
+  const viewMode = document.querySelector('.ico')
+  const viewModeActive = viewMode.className.includes('list selected') ? 'list' : 'gallery'
 
-	if (viewModeActive === 'gallery') {
-		soldQuantitySelector = '.sold-quantity'
-		locationSelector = '.info-shipping'
-		listSelector = '.gallery-large'
-	} else if (viewModeActive === 'list') {
-		soldQuantitySelector = '.extra-info-sold'
-		locationSelector = '.extra-info-location'
-		listSelector = '.list-view'
-	}
+  if (viewModeActive === 'gallery') {
+    soldQuantitySelector = '.sold-quantity'
+    locationSelector = '.info-shipping'
+    listSelector = '.gallery-large'
+  } else if (viewModeActive === 'list') {
+    soldQuantitySelector = '.extra-info-sold'
+    locationSelector = '.extra-info-location'
+    listSelector = '.list-view'
+  }
 
   // Add a li element "0 vendidos" to those products that didn't sell nothing yet
-	const rowItems = [...document.querySelectorAll('.results-item')]
-	rowItems.forEach(row => {
-		const info = row.querySelector(soldQuantitySelector)
-		if (!info) {
-			row
+  const rowItems = [...document.querySelectorAll('.results-item')]
+  rowItems.forEach(row => {
+    const info = row.querySelector(soldQuantitySelector)
+    if (!info) {
+      row
         .querySelector(locationSelector)
         .insertAdjacentHTML('beforebegin', `<li class="${soldQuantitySelector.replace('.', '')}">0 vendidos</li>`)
-		}
-	})
+    }
+  })
 
   // Sort all the quantity of sold products descendly
-	const rowItemsUpdated = [...document.querySelectorAll('.results-item')]
-	const rowItemsSorted = rowItemsUpdated.sort((a, b) => {
-		const valueA = a.querySelector(soldQuantitySelector).innerText.replace(/\D/g, '')
-		const valueB = b.querySelector(soldQuantitySelector).innerText.replace(/\D/g, '')
-		return valueA - valueB
-	})
+  const rowItemsUpdated = [...document.querySelectorAll('.results-item')]
+  const rowItemsSorted = rowItemsUpdated.sort((a, b) => {
+    const valueA = a.querySelector(soldQuantitySelector).innerText.replace(/\D/g, '')
+    const valueB = b.querySelector(soldQuantitySelector).innerText.replace(/\D/g, '')
+    return valueA - valueB
+  })
 
   // Clean all items inside the list
-	const listView = document.querySelector(listSelector)
-	listView.innerHTML = null
+  const listView = document.querySelector(listSelector)
+  listView.innerHTML = null
 
   // Insert all the items sorted descendly
-	rowItemsSorted.forEach(row => listView.insertAdjacentElement('afterbegin', row))
+  rowItemsSorted.forEach(row => listView.insertAdjacentElement('afterbegin', row))
 }
