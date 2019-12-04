463cf1bf53e37bb5b1ed61a4d01a424509107c24
Leonardo Galante <lndgalante@gmail.com>
2019-12-04 13:16:14 -0300

    Add API call

--- src/content-script.js
+++ src/content-script.js
@@ -2,10 +2,22 @@
 /* eslint no-undef: "error" */
 
 chrome.runtime.sendMessage('getState', state => {
-  if (state) {
-    sortItems()
-  }
+  if (state) sortItems()
 })
+
+/* Helpers */
+const chunk = (arr, size) => {
+  return Array.from({ length: Math.ceil(arr.length / size) }, (v, i) => arr.slice(i * size, i * size + size))
+}
+
+const flatten = array => array.reduce((acc, arr) => [...acc, ...arr], [])
+
+const getItemId = item =>
+  (item
+    .querySelector('.list-view-item-title a')
+    .getAttribute('href')
+    .replace(/-/g, '')
+    .match(/MLA\d*/g) || [''])[0]
 
 function sortItems() {
   // Declare ids and class selectors
@@ -13,31 +25,57 @@ function sortItems() {
   const itemSelector = '.results-item'
   const soldQuantitySelector = '.item__condition'
 
-  // Add a li element "0 vendidos" to those products that didn't sell nothing yet
-  const rowItems = [...document.querySelectorAll(soldQuantitySelector)]
-  rowItems.forEach(row => {
-    const text = row.innerText
-    if (!text.includes('vendido')) {
-      row.innerText = `0 vendidos - ${text}`
-    }
-  })
+  // Get all elements IDs
+  const rowItems = [...document.querySelectorAll(itemSelector)]
+  const ids = rowItems.map(rowItem => getItemId(rowItem)).filter(id => !id || id !== 'MLA')
+  console.log('TCL: sortItems -> ids', ids)
 
-  // Sort all the quantity of sold products descendly
-  const rowItemsUpdated = [...document.querySelectorAll(itemSelector)]
-  const rowItemsSorted = rowItemsUpdated.sort((a, b) => {
-    const valueA = a.querySelector(soldQuantitySelector).innerText.replace(/\D/g, '')
+  // Create chunks every 20 items, since ML API can only receive until 20 ids
+  const idChunks = chunk(ids, 20)
 
-    const valueB = b.querySelector(soldQuantitySelector).innerText.replace(/\D/g, '')
+  // Trigger parallels fetchs
+  const responses = Promise.all(
+    idChunks.map(idChunk =>
+      fetch(`https://api.mercadolibre.com/items?ids=${idChunk.join(',')}&attributes=id,sold_quantity`)
+        .then(res => res.json())
+        .then(data => {
+          console.log('TCL: sortItems -> data', data)
+          return data.map(item => ({
+            id: item.body.hasOwnProperty('id') ? item.body.id : 0,
+            soldQuantity: item.body.hasOwnProperty('sold_quantity') ? item.body.sold_quantity : 0,
+          }))
+        })
+    )
+  )
 
-    return valueA - valueB
-  })
+  // Now that we have the data we have to insert it on the DOM
+  responses.then(data => {
+    const flattenData = flatten(data)
 
-  // Clean all items inside the list
-  const listView = document.querySelector(listSelector)
-  listView.innerHTML = null
+    rowItems.forEach(rowItem => {
+      const id = getItemId(rowItem)
+      const item = flattenData.find(({ id: itemId }) => itemId === id) || { soldQuantity: 0 }
 
-  // Insert all the items sorted descendly
-  rowItemsSorted.forEach(row => {
-    listView.insertAdjacentElement('afterbegin', row)
+      const $soldQuantity = rowItem.querySelector(soldQuantitySelector)
+      $soldQuantity.textContent = `${item.soldQuantity} vendidos - ${$soldQuantity.textContent}`
+    })
+
+    // Sort all the quantity of sold products descendly
+    const rowItemsUpdated = [...document.querySelectorAll(itemSelector)]
+    const rowItemsSorted = rowItemsUpdated.sort((a, b) => {
+      const valueA = a.querySelector(soldQuantitySelector).innerText.replace(/\D/g, '')
+      const valueB = b.querySelector(soldQuantitySelector).innerText.replace(/\D/g, '')
+
+      return valueA - valueB
+    })
+
+    // Clean all items inside the list
+    const listView = document.querySelector(listSelector)
+    listView.innerHTML = null
+
+    // Insert all the items sorted descendly
+    rowItemsSorted.forEach(row => {
+      listView.insertAdjacentElement('afterbegin', row)
+    })
   })
 }
